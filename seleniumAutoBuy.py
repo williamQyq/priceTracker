331794bd058b3cec82c3526a4525afcb934d2021
DESKTOP-TL38TAD\W <qyqfiles@hotmail.com>
2021-06-02 18:05:53 -0400

    .

--- seleniumAutoBuy.py
+++ seleniumAutoBuy.py
@@ -4,35 +4,24 @@ from mypackage.init import init_excel, init_chrome_driver
 
 
 def main():
-    try:
-        try_caluse()
-    except:
-        print("main try except error")
-    # df = init_excel('Product_Tracker.xlsx','TrackProduct')
-    # driver=init_chrome_driver()
     
-    # infinite = 0
-    # while(infinite<1):
-    #     price_reach = track_instock_price(df,driver)
+    df = init_excel('Product_Tracker.xlsx','TrackProduct')
+    driver=init_chrome_driver()
+    
+    infinite = 0
+    while(infinite<1):
+        price_reach = track_instock_price(df,driver)
 
-    #     if price_reach:
-    #         try:
-    #             iterate_quests(df,driver)
-    #         except: 
-    #             print("failed")
+        if price_reach:
+            try:
+                iterate_quests(df,driver)
+            except: 
+                print("failed")
 
-    #         print("[Status]: === Finished, output result===\n")
-    #     infinite+=1
+            print("[Status]: === Finished, output result===\n")
+        infinite+=1
 
-    # driver.close()
+    driver.close()
 
-def try_caluse():
-    try:
-        a = 100/0
-        b = 100/2
-        print(b)
-    except:
-        print("error")
-    a = 100/0
 
 main()
